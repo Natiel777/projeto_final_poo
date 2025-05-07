5399e10eec8808b196bdb4c182ba2d2b54251784
franciscoxavier09aluno <francisco.xavier09@aluno.ifce.edu.br>
2025-05-07 20:01:23 -0300

    Update disciplina.py

--- disciplina.py
+++ disciplina.py
@@ -1,22 +1,29 @@
+from persistencia import Persistencia
 class Disciplina:
-    def __init__(self, codigo, nome, professor_responsavel):
-        self.codigo = codigo
+    def __init__(self, disciplina, nome, professor_responsavel):
+        self.disciplina = disciplina
         self.nome = nome
         self.professor_responsavel = professor_responsavel
         self.alunos_matriculados = []
 
     def exibir_dados(self):
-        print(f"Disciplina: {self.nome} ({self.codigo})")
-        print(f"Professor Responsável: {self.professor_responsavel.nome}")
-        print("Alunos Matriculados:")
-        for aluno in self.alunos_matriculados:
+            print(f"Disciplina: {self.nome} ({self.disciplina})")
+        if self.professor_responsavel:
+            print(f"Professor Responsável: {self.professor_responsavel.nome}")
+        else:
+            print("Professor Responsável: Não atribuído")
+            print("Alunos Matriculados:")
+        if self.alunos_matriculados:
+            for aluno in self.alunos_matriculados:
             print(f"  - {aluno.nome}")
+        else:
+            print("Nenhum aluno matriculado.")
 
     def adicionar_aluno(self, aluno):
         if aluno not in self.alunos_matriculados:
-            self.alunos_matriculados.append(aluno)
+           self.alunos_matriculados.append(aluno)
         if self not in aluno.disciplinas:
-            aluno.disciplinas.append(self)
+           aluno.disciplinas.append(self)
 
     def atribuir_ao_professor(self):
         if self not in self.professor_responsavel.disciplinas:
